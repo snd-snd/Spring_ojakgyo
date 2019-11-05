714bb8e5195b575a8e1af4d1a58feabb2e8a28e3
kimtaeho <hyssop9734@naver.com>
2019-11-05 17:40:55 +0900

    게시판 댓글의 답글 준비

--- ojakgyo_javaDB.sql
+++ ojakgyo_javaDB.sql
@@ -3,7 +3,7 @@ create table members( <!-- 회원정보 -->
 	userId varchar2(50) not null,
 	userPw varchar2(50) not null,
 	userName varchar2(50) not null,
-	nickName varchar2(50) not null,
+	nickName varchar2(50) not null unique,
 	email varchar2(100) not null,
 	groupCode1 varchar2(100),
 	groupCode2 varchar2(100),
@@ -21,7 +21,7 @@ create table groups( <!-- 그룹 생성시 저장되는 정보 -->
 	regDate date default sysdate,
 	status number default 2,
 	headCount number(10,0)
-	constraint fk_leader foreign key (leader) references members (userId)
+	constraint fk_leader foreign key (leader) references members (nickName)
 );
 create sequence seq_groups; <!-- 그룹 테이블의 gno를 위한 시퀀스 -->
 
